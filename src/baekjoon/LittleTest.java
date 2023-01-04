f2ef693d2f7ff775033ece90bd3279769d8fd0d0
bum19 <1996joon@gmail.com>
2023-01-04 23:30:20 +0900

    테스트용 자바파일

---
+++ src/baekjoon/LittleTest.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class LittleTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String tmp = sc.next();
+        tmp += "??!";
+        System.out.println(tmp);
+    }
+}
